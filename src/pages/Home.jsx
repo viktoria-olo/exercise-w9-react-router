362d559001f33a303aaa12649bccb1ac710f3957
Viktoria Olofsson <viktoriaolofsson@gmail.com>
2024-04-02 14:33:15 +0200

    Co-authored-by: jyy009 <jyy009@users.noreply.github.com> Co-authored-by: Maja Schouenke <schouenkes@users.noreply.github.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,25 @@
 // TODO: Import necessary modules and data
-function Home() {
+import Songs from "../data/songs.json";
+import { Link } from "react-router-dom";
+const Home = () => {
   // TODO: Implement logic to display the list of songs
+  const songsData = Songs.songs;
   return (
     <>
       <div>
         <h1>Rolling Stones 500 Greatest Songs of all Time</h1>
-        {/* TODO: Display the list of songs with links */}
+        <ul className="app-ul">
+          {songsData.map((song) => (
+            <li key={song.rank}>
+              <Link to={`/song/${song.title.toLowerCase().replace(/ /g, "-")}`}>
+                {song.title} - {song.artist}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
-}
+};
 
 export default Home;
--- src/pages/SongInfo.jsx
+++ src/pages/SongInfo.jsx
@@ -1,17 +1,29 @@
 // TODO: Import necessary modules and data
+import { useParams } from "react-router-dom";
+import Songs from "../data/songs.json";
 
 // TODO: Destrcutrue props to receive information coming in from the parent comp
-function SongInfo() {
-  // TODO: Fetch the song title from the URL and find the corresponding song data and add a conditional to display some simple html if there is no song :)
+const SongInfo = () => {
+  const { songTitle } = useParams();
 
+  // TODO: Fetch the song title from the URL and find the corresponding song data and add a conditional to display some simple html if there is no song :)
+  const songInfo = Songs.songs.find(
+    (s) => s.title.toLowerCase().replace(/ /g, "-") === songTitle
+  );
+  if (!songInfo) {
+    return <div>Song not found!</div>;
+  }
   return (
     <>
+      {/* TODO: Display song details or a "Song not found!" message */}
       <div>
-        <h2>Song Information</h2>
-        {/* TODO: Display song details or a "Song not found!" message */}
+        <h2>{songInfo.title}</h2>
+        <h2>{songInfo.artist}</h2>
+        <h2>{songInfo.album}</h2>
+        <h2>{songInfo.year}</h2>
       </div>
     </>
   );
-}
+};
 
 export default SongInfo;
